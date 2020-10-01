e52fc4b4f544ccba8abf08536d4124f7e14dc8f3
repl.it user <replituser@example.com>
2020-10-01 11:10:10 +0000

    weekone

---
+++ main.py
@@ -0,0 +1,14 @@
+# Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
+# Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
+        ##########
+#  o  o  #
+#  -
